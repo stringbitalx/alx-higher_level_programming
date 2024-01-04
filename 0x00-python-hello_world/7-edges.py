994fbee496d717a49b4dd7a96deb819887b5f635
stringbitalx <stringbitalx@gmail.com>
2024-01-03 22:17:48 -0800

    final updates

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ first_3_words = word[:3]
 last_2_words = word[-2:]
 middle_word = word[1:-1]
 print("First 3 letters: {}".format(first_3_words))
-print("Last 2 letter: {}".format(last_2_words))
+print("Last 2 letters: {}".format(last_2_words))
 print("Middle word: {}".format(middle_word))
