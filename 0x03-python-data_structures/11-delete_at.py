78bd295ee61953f60c028a2f6b0a5a637ada362f
stringbitalx <stringbitalx@gmail.com>
2024-01-08 11:41:46 -0800

    final updates

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@
 
 def delete_at(my_list=[], idx=0):
     """Delete an item at a specific position"""
-    if idx >= 0 and idx <= len(my_list):
+    if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return (my_list)
