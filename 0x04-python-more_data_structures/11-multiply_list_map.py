f61cdab55ded0253336cac39a6dd3a67a8617d41
stringbitalx <stringbitalx@gmail.com>
2024-01-10 11:26:24 -0800

    final updates

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 11-multiply_list_map.py
 
-def multiply_list_map(my_lis=[], number=0):
+def multiply_list_map(my_list=[], number=0):
     return (list(map((lambda i: i * number), my_list)))
