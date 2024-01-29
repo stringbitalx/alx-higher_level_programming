3af93b09385503e613858401bf8260b04e2b0d6b
stringbitalx <stringbitalx@gmail.com>
2024-01-29 04:25:19 -0800

    3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,7 +11,7 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >=0")
         self.__size = size
 
     def area(self):
