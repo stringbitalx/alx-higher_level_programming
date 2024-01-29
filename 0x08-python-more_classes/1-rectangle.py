d7b6c63b0efca29637e70dd44032d131cce6da84
stringbitalx <stringbitalx@gmail.com>
2024-01-29 04:02:05 -0800

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
 
         self.__width = value
 
@@ -34,12 +34,12 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """set new height attribure"""
+        """set new height attribte"""
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
 
         self.__height = value
