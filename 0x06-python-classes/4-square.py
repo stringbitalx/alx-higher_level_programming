dba27b3cc0243af5a515dcd8ebe3ce8f0a2bce92
stringbitalx <stringbitalx@gmail.com>
2024-01-28 23:44:40 -0800

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,11 +8,13 @@ class Square:
     def __init__(self, size=0):
         """init the data"""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+
         self.__size = size
+
+    @property
+    def size(self):
+        """return size of square"""
+        return self.__size
 
     def area(self):
         """return the current square area"""
