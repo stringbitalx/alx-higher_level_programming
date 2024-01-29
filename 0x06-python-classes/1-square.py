8172cbb31993d18876581aa78d244ff46c643f9d
stringbitalx <stringbitalx@gmail.com>
2024-01-28 23:25:46 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define new Class Square"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+        """Initializes attributes for the square"""
+
+        self.__size = size
