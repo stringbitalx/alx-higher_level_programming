3f0157408af21d45790af20751274a33728ee97d
stringbitalx <stringbitalx@gmail.com>
2024-02-06 08:16:20 -0800

    final update

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""add items module"""
+import json
+import sys
+import os.path
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+mylist = []
+file = "add_item.json"
+
+if os.paths.exists(file):
+    mylist = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    mylist.append(sys.argv[i])
+
+save_to_json_file(mylist, file)
